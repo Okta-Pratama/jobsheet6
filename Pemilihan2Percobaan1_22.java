72c364941d21f33d86875ff204283e5bafbf0b99
Okta Pratama <oktaiwp849@gmail.com>
2024-10-04 09:05:06 +0700

    last

--- Pemilihan2Percobaan1_22.java
+++ Pemilihan2Percobaan1_22.java
@@ -2,18 +2,25 @@ import java.util.Scanner;
 
 public class Pemilihan2Percobaan1_22 {
     public static void main(String[] args) {
-        Scanner inputAbsen = new Scanner(System.in);
+        Scanner inputTahun = new Scanner(System.in);
 
         System.out.print("Masukkan tahun: ");
-        int tahun = inputAbsen.nextInt();
+        int tahun = inputTahun.nextInt();
 
-        if ((tahun % 4 )== 0) {
-            if ((tahun % 100)!= 0)
-                System.out.println("tahun kabisat.");
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0){
+                if (tahun % 400 == 0){
+                    System.out.println("tahun kabisat.");
+                } else {
+                    System.out.println("bukan tahun kabisat.");
+                }
+            } else {
+                System.out.println("bukan tahun kabisat.");
+            }
         } else {
             System.out.println("bukan tahun kabisat.");
         }
 
-        inputAbsen.close();
+        inputTahun.close();
     }
 }
